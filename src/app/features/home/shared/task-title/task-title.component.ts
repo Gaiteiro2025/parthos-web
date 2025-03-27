e2f3ffe9ba17d3333cfaed3ad36d4c9729168469
Gaiteiro2025 <allansgaiteiro+dev@gmail.com>
2025-03-26 21:12:05 -0300

    criado componente estilizado do titulo

--- src/app/features/home/shared/task-title/task-title.component.ts
+++ src/app/features/home/shared/task-title/task-title.component.ts
@@ -8,21 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
   selector: 'app-task-title',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, MatIconModule, MatButtonModule],
-  template: `
-  <div class="task-title-container">
-    <div class="title-wrapper" *ngIf="!editing" (click)="toggleEditing()">
-      <span class="task-title">{{ getFormControl().value || 'Nova Tarefa' }}</span>
-      <mat-icon class="edit-icon">edit</mat-icon>
-    </div>
-
-    <div *ngIf="editing" class="edit-wrapper">
-      <input #titleInput class="task-title-input" [formControl]="getFormControl()" (blur)="saveOrCancelEdit()" autofocus />
-      <button mat-icon-button color="warn" (mousedown)="cancelEditing($event)">
-        <mat-icon>close</mat-icon>
-      </button>
-    </div>
-  </div>
-  `,
+  templateUrl: './task-title.component.html',
   styleUrls: ['./task-title.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
